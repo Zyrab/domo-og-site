e6175f96046077dc49dc5fde810d6b05cc29e02d
zyrab <zurakrua@gmail.com>
2026-03-17 22:47:48 +0100

    feat: added content for header and hero

---
+++ src/assets/locales/ge/content.js
@@ -0,0 +1,13 @@
+export const content = {
+  header: {
+    nav: [
+      { text: "მთავარი", varinat: "nav-link", href: "/" },
+      { text: "დოკუმენტაცია", varinat: "nav-link", href: "/docs" },
+      { text: "დემო", varinat: "nav-link", href: "/#playground" },
+    ],
+    buttons: [
+      { icon: "npm", label: "NPM", variant: "dark", href: "https://www.npmjs.com/package/@zyrab/domo-og" },
+      { icon: "github", label: "Github", href: "https://github.com/zyrab/domo/tree/main/packages/domo-og" },
+    ],
+  },
+};
