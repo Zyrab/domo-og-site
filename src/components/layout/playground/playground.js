f567e95f80100d4aba2ca20d203be651d8f2d356
zyrab <zurakrua@gmail.com>
2026-03-21 19:19:30 +0100

    feat: playground section . island parent of webste

--- src/components/layout/playground/playground.js
+++ src/components/layout/playground/playground.js
@@ -1,69 +1,34 @@
 // src/components/layout/playground/playground.js
 import Domo from "@zyrab/domo";
+
 import createButton from "../../ui/button/button.js";
-import createTemplateCode from "./template-code.js";
-import createTemplatePreview from "./template-preview.js";
-import createTemplateEditor from "./template-editor.js";
-import { templates } from "./templates.js";
+import createIcon from "../../ui/icon.js";
 import createCodeCopy from "../../composed/code-copy.js";
-import createTemplateSwitcher from "./template-editor-switcher.js";
+import createTooltip from "../../ui/tooltip/tooltip.js";
+
+import createEditorView from "./islands/editor-view.js";
+import createPreviewView from "./islands/preview-view.js";
+
+import { templates } from "./templates.js";
+
 import getLocales from "../../../assets/locales/get-locale.js";
-// --- TEMPLATE DATA ---
 
 export default function createPlayground() {
   const t = getLocales("en", "playground");
   let currentTemplateId = "blog";
 
-  let templateCodeDisplay = createTemplateCode(templates.blog);
-  let templateEditorDisplay = createTemplateEditor(templates.blog);
-  let templatePreviewDisplay = createTemplatePreview(templates.blog);
-
-
-  function updateDisplays(templateId) {
-    const templateData = templates[templateId];
-
-    const codeNode = createTemplateCode(templateData);
-    const isEditing = templateCodeDisplay.element.classList.contains("hidden");
-    templateCodeDisplay.replace(templateCodeDisplay.build(), codeNode.build());
-    templateCodeDisplay = codeNode.cls(isEditing ? "hidden" : "");
-
-    const editorNode = createTemplateEditor(templateData);
-    templateEditorDisplay.replace(templateEditorDisplay.build(), editorNode.build());
-    templateEditorDisplay = editorNode.cls(isEditing ? "" : "hidden");
-
-    const previewNode = createTemplatePreview(templateData);
-    templatePreviewDisplay.replace(templatePreviewDisplay.build(), previewNode.build());
-    templatePreviewDisplay = previewNode;
-  }
-
-  function changeTemplate(e, t) {
-    const parent = t.parentNode;
-    const active = parent.querySelector(".active");
-    if (active) active.classList.remove("active");
-
-    t.classList.add("active");
-    currentTemplateId = t.dataset.template;
-    updateDisplays(currentTemplateId);
-  }
-
-
-  function handleTemplateUpdate(e) {
-    const newTemplateData = e.detail;
-    const newPreviewNode = createTemplatePreview(newTemplateData);
-    templatePreviewDisplay.replace(templatePreviewDisplay.build(), newPreviewNode.build());
-    templatePreviewDisplay = newPreviewNode;
-    const newCodeNode = createTemplateCode(newTemplateData);
-    templateCodeDisplay.replace(templateCodeDisplay.build(), newCodeNode.build());
-    templateCodeDisplay = newCodeNode.cls("hidden");
-  }
-
-
-
   const templateString = ` {\n"const template" = ${JSON.stringify(templates[currentTemplateId], null, 2)}\n}`;
 
+  const switchEditorView = () => {
+    const { tooltip, button, edit, object } = t.editor_switcher;
+    return createTooltip({
+      ...tooltip,
+      child: [createButton({ ...button, child: [createIcon(edit), createIcon(object)] })],
+    });
+  };
   return Domo("section")
-    .cls("playground")
     .id("playground")
+    .cls("playground")
     .child([
       Domo()
         .cls("playground__container")
@@ -76,15 +41,18 @@ export default function createPlayground() {
                 .child([
                   Domo("h2").cls("playground__title heading-bold").txt(t.title),
                   Domo("p").cls("playground__subtitle").txt(t.subtitle),
-                  Domo("p")
-                    .css({ fontSize: "0.8rem", opacity: 0.6, marginTop: "4px" })
-                    .txt(t.note),
+                  Domo("p").css({ fontSize: "0.8rem", opacity: 0.6, marginTop: "4px" }).txt(t.note),
                 ]),
               Domo()
                 .cls("playground__template-list")
                 .child(
                   t.template_buttons.map(({ label, key }) =>
-                    createButton({ label, variant: "dark", "data-template": key, cls: "change-template" }).cls(key === currentTemplateId ? "active" : ""),
+                    createButton({
+                      label,
+                      variant: "dark",
+                      "data-template": JSON.stringify(templates[key], null, 2),
+                      cls: "change-template",
+                    }).cls(key === currentTemplateId ? "active" : ""),
                   ),
                 ),
             ]),
@@ -101,32 +69,26 @@ export default function createPlayground() {
                       Domo("div")
                         .cls("playground__tabs")
                         .child([
-                          createTemplateSwitcher(t.editor_switcher),
+                          switchEditorView(),
                           createCodeCopy({
                             ...t.code_copy,
                             button: { ...t.code_copy.button, "data-code": templateString },
                           }),
                         ]),
                     ]),
-                  templateCodeDisplay,
-                  templateEditorDisplay.cls("hidden"),
                 ])
                 .id("template-editor-pane")
-                .on("template-update", handleTemplateUpdate),
+                .island(createEditorView),
 
-              // RIGHT PANE
               Domo("div")
                 .cls("playground__preview-pane")
                 .child([
                   Domo("div")
                     .cls("playground__editor-header")
                     .child([Domo("h3").cls("title").txt(t.og_preview)]),
-                  templatePreviewDisplay,
-                ]),
+                ])
+                .island(createPreviewView),
             ]),
         ]),
-    ])
-    .onClosest("click", {
-      ".change-template": changeTemplate,
-    });
+    ]);
 }
