83d1418db46ffe8e0a0f67a7de72a9e35e742780
zyrab <zurakrua@gmail.com>
2026-03-21 18:24:23 +0100

    ref: updared routes files import in main

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
 // src/main.js
-import Domo from "@zyrab/domo";
 import Router from "@zyrab/domo-router";
-import routes from "./routes.js";
+import { routes } from "./routes.js";
 
 function initApp() {
-    Router.routes(routes);
-    Router.listen(({ meta }) => {
-        document.title = meta?.title || "Domo-OG";
-        window.scrollTo(0, 0);
-    });
-    Router.init();
-    Router.mount();
+  Router.routes(routes);
+  Router.listen(({ meta }) => {
+    document.title = meta?.title || "Domo-OG";
+    window.scrollTo(0, 0);
+  });
+  Router.init();
+  Router.mount();
 }
 
 initApp();
