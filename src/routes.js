97539bca440a314fe4d5f43cd64154947227b49e
zyrab <zurakrua@gmail.com>
2026-03-21 18:24:04 +0100

    ref: updated router

--- src/routes.js
+++ src/routes.js
@@ -6,23 +6,24 @@ import createFooter from "./components/layout/footer/footer.js";
 import createVersionBar from "./components/layout/version-bar/version-bar.js";
 
 export const routes = {
-    // layouts: {
-    //     default: (c) => {
-    //         const frag = document.createDocumentFragment();
-    //         frag.appendChild(createHeader().build());
-    //         frag.appendChild(createVersionBar().build());
-    //         frag.appendChild(c);
-    //         frag.appendChild(createFooter().build());
-    //         return frag;
-    //     },
-    // },
-    "/": {
-        component: createLandingPage,
-        meta: { title: "Domo-OG - A blazing-fast Open Graph image generator", description: "Design with data, not DOM nodes." },
+  layouts: {
+    default: (c) => {
+      const frag = document.createDocumentFragment();
+      frag.appendChild(createHeader().build());
+      frag.appendChild(c);
+      frag.appendChild(createFooter().build());
+      return frag;
     },
-    "/docs": {
-        component: createDocsPage,
-        meta: { title: "Domo-OG Documentation", description: "Documentation for Domo-OG." },
+  },
+  "/": {
+    component: createLandingPage,
+    meta: {
+      title: "Domo-OG - A blazing-fast Open Graph image generator",
+      description: "Design with data, not DOM nodes.",
     },
+  },
+  "/docs": {
+    component: createDocsPage,
+    meta: { title: "Domo-OG Documentation", description: "Documentation for Domo-OG." },
+  },
 };
-
