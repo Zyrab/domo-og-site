79512d447534bc738f886e9b10370a8f21f4058e
zyrab <zurakrua@gmail.com>
2026-03-21 18:28:16 +0100

    ref: updaetd mini code higliter

--- src/components/ui/code-highlight/code-highlight.js
+++ src/components/ui/code-highlight/code-highlight.js
@@ -1,4 +1,3 @@
-// src/components/ui/code-highlight/code-highlight.js
 import Domo from "@zyrab/domo";
 
 export default function createCodeHighlight(props = {}) {
@@ -7,15 +6,15 @@ export default function createCodeHighlight(props = {}) {
   // Simple tokenizer logic
   const children = [];
   const regex = /("[^"]*")|(\/\/.*)|\b(import|from|await|export|const|function)\b|([\{\}\[\]\(\),;:\.])/g;
-  
+
   let match;
   let currentIndex = 0;
-  
+
   while ((match = regex.exec(code)) !== null) {
     if (match.index > currentIndex) {
       children.push(Domo("span").txt(code.substring(currentIndex, match.index)));
     }
-    
+
     if (match[1]) {
       children.push(Domo("span").cls("token-string").txt(match[1]));
     } else if (match[2]) {
@@ -25,17 +24,15 @@ export default function createCodeHighlight(props = {}) {
     } else if (match[4]) {
       children.push(Domo("span").cls("token-operator").txt(match[4]));
     }
-    
+
     currentIndex = regex.lastIndex;
   }
-  
+
   if (currentIndex < code.length) {
     children.push(Domo("span").txt(code.substring(currentIndex)));
   }
 
-  const el = Domo("pre")
-    .cls("code-highlight")
-    .child(children);
+  const el = Domo("pre").cls("code-highlight").child(children);
 
   return el;
 }
