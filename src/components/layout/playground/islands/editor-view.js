a21cccf2756e8843fb1277176766b45b1094e3a2
zyrab <zurakrua@gmail.com>
2026-03-21 19:18:35 +0100

    feat: editor island

---
+++ src/components/layout/playground/islands/editor-view.js
@@ -0,0 +1,11 @@
+import Domo from "@zyrab/domo";
+import { defultTemplate } from "./defaut-template.js";
+import createTemplateCode from "./template-code.js";
+import createTemplateEditor from "./template-editor.js";
+
+export default function createEditorView() {
+  const frag = document.createDocumentFragment();
+  frag.append(createTemplateCode(defultTemplate).build());
+  frag.append(createTemplateEditor(defultTemplate).cls("hidden").build());
+  return frag;
+}
