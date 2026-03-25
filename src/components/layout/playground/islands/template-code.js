4d6f12f0275a6dc0f32a4d72b8acbc8f62f88e07
zyrab <zurakrua@gmail.com>
2026-03-25 20:26:09 +0100

    ref: updated template decoder

--- src/components/layout/playground/islands/template-code.js
+++ src/components/layout/playground/islands/template-code.js
@@ -4,8 +4,6 @@ import createCodeHighlight from "../../../ui/code-highlight.js";
 export default function createTemplateCode(template) {
   let code = "";
   if (typeof template === "string") {
-    // const str = template.toString();
-    // const result = str.slice(7, -1);
     code = `const template = ${template}`;
   } else {
     code = `const template = ${JSON.stringify(template, null, 2)}`;
